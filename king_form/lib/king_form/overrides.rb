fb390e65ec68f53a1da4f7593bb54f498e0971e8
Georg Leciejewski <gl@salesking.eu>
2012-08-12 13:58:36 +0200

    commented king_form overrides

--- king_form/lib/king_form/overrides.rb
+++ king_form/lib/king_form/overrides.rb
@@ -9,17 +9,17 @@ module ActionView::Helpers::FormTagHelper
   private
   # overridden method to kill inline styles
   # from actionpack-2.1.0/lib/action_view/helpers/form_tag_helper.rb
-  def extra_tags_for_form(html_options)
-    case method = html_options.delete("method").to_s
-      when /^get$/i # must be case-insentive, but can't use downcase as might be nil
-        html_options["method"] = "get"
-        ''
-      when /^post$/i, "", nil
-        html_options["method"] = "post"
-        protect_against_forgery? ? content_tag(:div, token_tag) : ''
-      else
-        html_options["method"] = "post"
-        content_tag(:div, tag(:input, :type => "hidden", :name => "_method", :value => method) + token_tag)
-    end
-  end
+  #def extra_tags_for_form(html_options)
+  #  case method = html_options.delete("method").to_s
+  #    when /^get$/i # must be case-insentive, but can't use downcase as might be nil
+  #      html_options["method"] = "get"
+  #      ''
+  #    when /^post$/i, "", nil
+  #      html_options["method"] = "post"
+  #      protect_against_forgery? ? content_tag(:div, token_tag) : ''
+  #    else
+  #      html_options["method"] = "post"
+  #      content_tag(:div, tag(:input, :type => "hidden", :name => "_method", :value => method) + token_tag)
+  #  end
+  #end
 end
